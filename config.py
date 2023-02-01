3f7905f4d83363506c57d50ab5646863f06f01a1
Trip4ikXD <116121097+Trip4ikXD@users.noreply.github.com>
2023-02-01 22:32:24 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "5911694429:AAGhJ75Aa-lbDCZ0YB-6wWq48SHF3Ald_e4"
-keys = {'Биткоин': 'BTC',
-        'Эфириум': 'ETH',
-        'Доллар': 'USD',
-        'Рубль': 'RUB',
-        'Евро': 'EUR'
-        }
